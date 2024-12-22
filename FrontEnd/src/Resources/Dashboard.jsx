8ffb340602234ec7312f1a1d06a7943430a53cb9
agaesh <agaeshk@gmail.com>
2024-12-22 19:11:57 +0800

    commiting all these changes

---
+++ FrontEnd/src/Resources/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'React'
+
+const DashBoard = () =>{
+    return(
+        <div>
+            <h1>Dashboard</h1>
+            <p>This is a dashboard page</p>
+        </div>
+    )
+}
+
+export default DashBoard;

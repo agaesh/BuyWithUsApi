9977433385fe24a3cecfa2b41bf3aab304f19185
agaesh <agaeshk@gmail.com>
2024-12-29 11:35:22 +0800

    Component: Implement routing in App component with LoginPage and DashBoard Date: 2023-10-06

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import LoginPage from './Resources/LoginRegister'
 import {BrowserRouter,Routes,Route} from 'react-router'
 import './App.css'
+import DashBoard from './Resources/Dashboard'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage />} />
+          <Route path = "/user-management" element={<DashBoard activePage={"user-management"} />} />
         </Routes>
     </BrowserRouter>
     </>

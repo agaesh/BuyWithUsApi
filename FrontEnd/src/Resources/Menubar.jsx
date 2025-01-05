5f2f7dc5d7104adbe4bb020f62787ecc8827044f
agaesh <agaeshk@gmail.com>
2025-01-05 09:51:45 +0800

    Feature: Implement dynamic navbar with authentication links and logout functionality

--- FrontEnd/src/Resources/Menubar.jsx
+++ FrontEnd/src/Resources/Menubar.jsx
@@ -1,22 +1,45 @@
 import React from "react";
 import '../assets/css/Navbar.css';
+import {useAuth} from '../Context/AuthContext';
 
 const Navbar = () => {
+  const {currentUser, logout} = useAuth();
+  const handleLogout = async () => {
+    try {
+      await logout();
+    }
+    catch (error) {
+      console.error("Error during logout:", error);
+    }
+  }
   return (
     <nav className="navbar">
       {/* Logo or Brand Name */}
       <div className="navbar-logo">
-  BUY<span style={{ color: "red" }}>WITHUS</span>
-</div>
+         BUY<span style={{ color: "red" }}>WITHUS</span>
+      </div>
 
       {/* Navbar Links */}
       <div className="navbar-links">
-        <a href="/register" className="navbar-link">
-          Register
-        </a>
-        <a href="/login" className="navbar-link">
-          Login
-        </a>
+        {currentUser?(
+          <>
+            <a href="/dashboard" className="navbar-link">
+              Dashboard
+            </a>
+            <button style ={{backgroundColor:"red", color: "white", padding:"6px"}} onClick={handleLogout} className="navbar-link">
+              Logout
+            </button>
+          </>       
+        ):(
+          <>
+            <a href="/register" className="navbar-link">
+            Register
+            </a>
+            <a href="/login" className="navbar-link">
+              Login
+            </a>
+          </> 
+        )}
       </div>
     </nav>
   );
